30f7438a28abfc25827c76646c0f412e78297c22
mariamelghandoor <mariamyasserelghandoor@gmail.com>
2023-12-14 22:39:40 +0200

    done mariam while 3yza tst7ma

---
+++ Pages/Cooker_interface.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project_DB.Pages
+{
+    public class Cooker_interfaceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
