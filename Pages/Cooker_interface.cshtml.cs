501003c67f12fbbd88de6f08315ece59f6717011
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-25 09:14:12 +0200

    ana 3ayza arawa7

--- Pages/Cooker_interface.cshtml.cs
+++ Pages/Cooker_interface.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace Project_DB.Pages
 {
+    [BindProperties]
     public class Cooker_interfaceModel : PageModel
     {
+        public int id { get; set; }
         public void OnGet()
         {
         }
+        public void OnPost() {
+            string connection = "Data Source=Tamer;Initial Catalog=\"Project 2.0\";Integrated Security=True";
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+
+
+		}
     }
 }
