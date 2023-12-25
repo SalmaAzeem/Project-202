3bb57c063a4540615dab1d9b9fa83c1020b4f744
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-25 11:48:15 +0200

    fe error fel cooker mahadsh yeftaho

--- Pages/Person.cs
+++ Pages/Person.cs
@@ -4,6 +4,7 @@ namespace Project_DB.Pages
 {
     public class Person
     {
+        public int Id { get; set; }
         public string UserName { get; set; } 
         public string Email { get; set; }
         public int Phone_Number { get; set; }
