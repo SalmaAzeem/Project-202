71296bc93c332a3171957d7a08319bcaf1967a8d
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-25 17:13:27 +0200

    cookers ba2et shaghala tany

--- Pages/Cookers.cshtml.cs
+++ Pages/Cookers.cshtml.cs
@@ -2,20 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
 using System.Data.SqlClient;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json;
 
 namespace Project_DB.Pages
 {
     [BindProperties]
     public class CookersModel : PageModel
     {
-        //public Person user = new Person();
-        public List<Person> cookers {  get; set; } = new List<Person>();
+        public string name { get; set; }
+        public List<Person> cookers { get; set; } = new List<Person>();
         public List<string> ids { get; set; } = new List<string>();
         public List<string> names { get; set; } = new List<string>();
-		public void OnGet()
-		{
+        public void OnGet()
+        {
             string connection = "Data Source=Tamer;Initial Catalog=\"Project 2.0\";Integrated Security=True";
             using (SqlConnection conn = new SqlConnection(connection))
             {
@@ -25,8 +23,8 @@ namespace Project_DB.Pages
                     string query = "select Cooker_id from Cooker";
                     using (SqlCommand cmd = new SqlCommand(query, conn))
                     {
-                        using (SqlDataReader reader = cmd.ExecuteReader()) 
-                        { 
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
                             while (reader.Read())
                             {
                                 ids.Add(reader["Cooker_id"].ToString());
@@ -36,7 +34,6 @@ namespace Project_DB.Pages
                     string query2 = "SELECT UserName, Email, Phone_Number FROM Userr WHERE ID = @Id";
                     using (SqlCommand cmd_2 = new SqlCommand(query2, conn))
                     {
-                        // Define the parameter outside the loop
                         cmd_2.Parameters.Add(new SqlParameter("@Id", SqlDbType.VarChar));
 
                         foreach (string id in ids)
@@ -48,11 +45,8 @@ namespace Project_DB.Pages
                                 while (reader_2.Read())
                                 {
                                     Person cooker = new Person();
-                                    //cooker.Id = Int32.Parse(id);
                                     cooker.UserName = reader_2["UserName"].ToString();
                                     cooker.Email = reader_2["Email"].ToString();
-                                    cooker.Phone_Number = (int)reader_2["Phone_Number"];
-                                    cookers.Add(cooker);
                                 }
                             }
                         }
