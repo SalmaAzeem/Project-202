a7c618ce9ba05b640f2a63d9ce418a56fce0871a
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-26 14:05:00 +0200

    done hewar el sowar bta3 el avatar bs still 3ayza ashoof hewar el blob

--- Pages/Cooker_interface.cshtml.cs
+++ Pages/Cooker_interface.cshtml.cs
@@ -7,14 +7,39 @@ namespace Project_DB.Pages
     [BindProperties]
     public class Cooker_interfaceModel : PageModel
     {
+		public string CookerGender { get; set; }
+		public int CookerID { get; set; }
 		public string CookerName { get; set; }
 		public string CookerEmail { get; set; }
 		public int CookerPhone { get; set; }
-		public void OnGet(string name, string email, string number)
+		//public string Image_path { get; set; }
+		public void OnGet(string name, string email, string phone, string id)
 		{
 			CookerName = name;
 			CookerEmail = email;
-			CookerPhone = Int32.Parse(number);
+			CookerPhone = Convert.ToInt32(phone);
+			CookerID = Convert.ToInt32(id);
+			string connection = "Data Source=Tamer;Initial Catalog=\"Project 2.0\";Integrated Security=True";
+			SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            string query = "select Gender from Cooker where Cooker_id = @CookerID";
+            try
+			{
+				SqlCommand cmd = new SqlCommand(query, con);
+				cmd.Parameters.AddWithValue("@CookerID", CookerID);
+				SqlDataReader reader = cmd.ExecuteReader();
+				if (reader.Read())
+				{
+					CookerGender = reader["Gender"].ToString();
+				}
+				
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occured: {ex.Message}");
+			}
+			finally { con.Close(); }
+			
 		}
 	}
 }
