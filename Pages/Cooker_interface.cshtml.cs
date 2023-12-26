a57f14a72fd7aad425d95711bfad4957db86bd57
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-26 23:19:49 +0200

    displayed the meals but have a problem with the handler method

--- Pages/Cooker_interface.cshtml.cs
+++ Pages/Cooker_interface.cshtml.cs
@@ -13,34 +13,57 @@ namespace Project_DB.Pages
 		public string CookerEmail { get; set; }
 		public int CookerPhone { get; set; }
 		public int Rating {  get; set; }
-		//public string Image_path { get; set; }
-		public void OnGet(string name, string email, string phone, string id)
+		public List<string> Meals { get; set; } = new List<string>();
+        //public string Image_path { get; set; }
+        public void OnGet(string name, string email, string phone, string id)
+        {
+            CookerName = name;
+            CookerEmail = email;
+            CookerPhone = Convert.ToInt32(phone);
+            CookerID = Convert.ToInt32(id);
+            string connection = "Data Source=Tamer;Initial Catalog=Project 2.0;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                try
+                {
+                    con.Open();
+                    //string query = "SELECT Gender FROM Cooker WHERE Cooker_id = @CookerID";
+                    //using (SqlCommand cmd = new SqlCommand(query, con))
+                    //{
+                    //    cmd.Parameters.AddWithValue("@CookerID", CookerID);
+                    //    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //    if (reader.Read())
+                    //    {
+                    //        CookerGender = reader["Gender"].ToString();
+                    //    }
+                    //}
+                    string query2 = "SELECT Meal_Name FROM Meals m INNER JOIN Cooks_Meal cm ON m.meal_id = cm.meal_id WHERE Cooker_id = @CookerID";
+                    using (SqlCommand cmd2 = new SqlCommand(query2, con))
+                    {
+                        cmd2.Parameters.AddWithValue("@CookerID", CookerID);
+                        SqlDataReader reader2 = cmd2.ExecuteReader();
+
+                        while (reader2.Read())
+                        {
+                            Meals.Add(reader2["Meal_Name"].ToString());
+                            Console.WriteLine($"Added meal: {reader2["Meal_Name"].ToString()}");
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally { con.Close(); }
+            }
+        }
+
+        public void OnGetMeals()
 		{
-			CookerName = name;
-			CookerEmail = email;
-			CookerPhone = Convert.ToInt32(phone);
-			CookerID = Convert.ToInt32(id);
-			string connection = "Data Source=Tamer;Initial Catalog=\"Project 2.0\";Integrated Security=True";
-			SqlConnection con = new SqlConnection(connection);
-            con.Open();
-            string query = "select Gender from Cooker where Cooker_id = @CookerID";
-            try
-			{
-				SqlCommand cmd = new SqlCommand(query, con);
-				cmd.Parameters.AddWithValue("@CookerID", CookerID);
-				SqlDataReader reader = cmd.ExecuteReader();
-				if (reader.Read())
-				{
-					CookerGender = reader["Gender"].ToString();
-				}
-				
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"An error occured: {ex.Message}");
-			}
-			finally { con.Close(); }
-			
+
 		}
 		public void OnPost(int ratingRadio)
 		{
