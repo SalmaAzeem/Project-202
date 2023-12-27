8b461b480401b56402904d5edbb7c1d7819e3723
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2023-12-27 15:07:57 +0200

    khaketo object msh variables

--- Pages/Cooker_interface.cshtml.cs
+++ Pages/Cooker_interface.cshtml.cs
@@ -7,23 +7,19 @@ namespace Project_DB.Pages
     [BindProperties(SupportsGet = true)]
     public class Cooker_interfaceModel : PageModel
     {
-		public int CookerID { get; set; }
         public int Rating_CookerID { get; set; }
-		public string CookerName { get; set; }
-		public string CookerEmail { get; set; }
-		public int CookerPhone { get; set; }
+        public Person Cooker = new Person();
 		public int Rating {  get; set; }
         public int Rating_Id { get; set; }
 		public List<string> Meals { get; set; } = new List<string>();
         Random random = new Random();
 
-        //public string Image_path { get; set; }
         public void OnGet(string name, string email, string phone, string id)
         {
-            CookerName = name;
-            CookerEmail = email;
-            CookerPhone = Convert.ToInt32(phone);
-            CookerID = Convert.ToInt32(id);
+            Cooker.UserName = name;
+            Cooker.Email = email;
+            Cooker.Phone_Number = Convert.ToInt32(phone);
+            Cooker.Id = Convert.ToInt32(id);
             
             string connection = "Data Source=Tamer;Initial Catalog=Project 2.0;Integrated Security=True";
 
@@ -35,7 +31,7 @@ namespace Project_DB.Pages
                     string query2 = "SELECT Meal_Name FROM Meals m INNER JOIN Cooks_Meal cm ON m.meal_id = cm.meal_id WHERE Cooker_id = @CookerID";
                     using (SqlCommand cmd2 = new SqlCommand(query2, con))
                     {
-                        cmd2.Parameters.AddWithValue("@CookerID", CookerID);
+                        cmd2.Parameters.AddWithValue("@CookerID", Cooker.Id);
                         SqlDataReader reader2 = cmd2.ExecuteReader();
 
                         while (reader2.Read())
