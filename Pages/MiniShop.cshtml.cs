94608cbcf1ed641689874b2f769f073bbd23a3f6
Doha Hassanien <202200701@zewailcity.edu.eg>
2023-12-30 15:44:03 +0200

    Added the pictures of Minishop and pictures of Meals and added koshary pic in the index

--- Pages/MiniShop.cshtml.cs
+++ Pages/MiniShop.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Project_DB.Pages
 {
@@ -14,6 +16,8 @@ namespace Project_DB.Pages
         public List<string> food_cans_names = new List<string>();
 
         public List<double> prices = new List<double>();
+        public List<string> ids_Minishop { get; set; } = new List<string>();
+        public List<byte[]> Images_Minishop { get; set; } = new List<byte[]>();
         public void OnGet()
         {
             string connectionString = "Data Source=Doha-PC;Initial Catalog=\"Project 2.0\";Integrated Security=True";
@@ -36,7 +40,7 @@ namespace Project_DB.Pages
             }
 
 
-            string querystring = "SELECT Food_cans, prices FROM MiniShop";
+            string querystring = "SELECT Food_cans, prices , minishop_id FROM MiniShop";
             try
             {
                 con.Open();
@@ -55,6 +59,7 @@ namespace Project_DB.Pages
                             price = Convert.ToDouble(reader[1]);
                             food_cans_names.Add(food_cans_name);
                             prices.Add(price);
+                            ids_Minishop.Add(reader[2].ToString());
 
                         }
                     }
@@ -75,7 +80,47 @@ namespace Project_DB.Pages
 
             }
         }
-
+        public async Task<IActionResult> OnGetImagesAsync()
+        {
+            string connection = "Data Source=Doha-PC;Initial Catalog=\"Project 2.0\";Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                await con.OpenAsync();
+                string query4 = "select MinisShop_Image from MiniShop where minishop_id = @Id";
+                using (SqlCommand cmd_4 = new SqlCommand(query4, con))
+                {
+                    cmd_4.Parameters.Add(new SqlParameter("@Id", SqlDbType.VarChar));
+                    foreach (string id in ids_Minishop)
+                    {
+                        cmd_4.Parameters["@Id"].Value = id;
+                        using (SqlDataReader reader_4 = await cmd_4.ExecuteReaderAsync())
+                        {
+                            if (await reader_4.ReadAsync())
+                            {
+                                const int buffersize = 4096;
+                                long bytesRead;
+                                long field_offset = 0; // Reset field_offset for each cooker
+                                long stream_length = reader_4.GetBytes(0, field_offset, null, 0, 0);
+                                using (MemoryStream ms = new MemoryStream())
+                                {
+                                    byte[] buffer = new byte[buffersize];
+                                    while ((bytesRead = reader_4.GetBytes(0, field_offset, buffer, 0, buffersize)) > 0)
+                                    {
+                                        await ms.WriteAsync(buffer, 0, (int)bytesRead);
+                                        field_offset += bytesRead;
+                                    }
+                                    Images_Minishop.Add(ms.ToArray());
+                                    Console.WriteLine(Images_Minishop.Count());
+                                    //Cooker_image = ms.ToArray();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return new EmptyResult();
         }
+
+    }
     }
 
