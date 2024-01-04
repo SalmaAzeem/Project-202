ceb04b1d901672cb3e12f08c1c8d06bb5d70767f
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2024-01-04 11:56:46 +0200

    backend bta3 el delivery azon done

--- Pages/DeliveryProfile.cshtml.cs
+++ Pages/DeliveryProfile.cshtml.cs
@@ -6,18 +6,17 @@ using System.Runtime;
 
 namespace Project_DB.Pages
 {
+    [BindProperties(SupportsGet = true)]
     public class DeliveryProfileModel : PageModel
     {
-        [BindProperty(SupportsGet = true)]
+        
         public int ID2 { get; set; }
-
-        [BindProperty(SupportsGet = true)]
         public Driver deliveryinfo { get; set; }
         public void OnGet()
         {
             try
             {
-                string connectionString = "Data Source =Doha-PC; Initial Catalog = Project 2.0; Integrated Security = True";
+                string connectionString = "Data Source =Tamer; Initial Catalog = Project 2.0; Integrated Security = True";
                 //deliveryinfo.Id = ID2;
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
@@ -46,6 +45,29 @@ namespace Project_DB.Pages
             {
                 Console.WriteLine(ex.ToString());
             }
+            try
+            {
+                string connection = "Data Source =Tamer; Initial Catalog = Project 2.0; Integrated Security = True";
+                using (SqlConnection con = new SqlConnection(connection))
+                {
+                    con.Open();
+                    string query_get_orders = "select Destination, city from Orders as o inner join Delivery as d on o.delivery_id = d.delivery_id where d.delivery_id = @Id and Destination is not null ";
+                    using (SqlCommand cmd = new SqlCommand (query_get_orders, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", ID2);
+                        using (SqlDataReader reader = cmd.ExecuteReader ())
+                        {
+                            if (reader.Read())
+                            {
+                                deliveryinfo.destination = reader["Destination"].ToString();
+                                deliveryinfo.city = reader["city"].ToString();
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
         }
 
 
